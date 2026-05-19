eef06d277bfa546a1ace9751014aeaff8bbf2d91
Vachagan <vach.mtx@gmail.com>
2026-05-19 20:32:30 +0400

    refactor: remove BeforeDashboard components and update package naming in import map

--- dev/app/(payload)/admin/importMap.js
+++ dev/app/(payload)/admin/importMap.js
@@ -1,10 +1,10 @@
-import { IconSelect as IconSelect_62cad1737be521ef9795d5ddc6531383 } from 'payload-icon-picker/client'
+import { IconSelect as IconSelect_9eb5387569f32230a7d5d13c56e48357 } from '@metax7/payload-icon-picker/client'
 import { IconPackProvider as IconPackProvider_1e89812ae8375dda6f44db5f624101c8 } from '../../../IconPackProvider'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 /** @type import('payload').ImportMap */
 export const importMap = {
-  "payload-icon-picker/client#IconSelect": IconSelect_62cad1737be521ef9795d5ddc6531383,
+  "@metax7/payload-icon-picker/client#IconSelect": IconSelect_9eb5387569f32230a7d5d13c56e48357,
   "./IconPackProvider#IconPackProvider": IconPackProvider_1e89812ae8375dda6f44db5f624101c8,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
--- src/exports/client.ts
+++ src/exports/client.ts
@@ -1,3 +1,2 @@
-export { BeforeDashboardClient } from '../components/BeforeDashboardClient.js'
 export { IconPackProvider } from '../components/IconPackContext.js'
 export { IconSelect } from '../components/IconSelect.js'
