28395ed89143a513dd2ab49d5d40a1ea10d18d1f
Vache <vach.mtx@gmail.com>
2026-06-16 16:33:47 +0400

    feat: implement IconRenderer helper component to replace dangerouslySetInnerHTML for SVG rendering

---
+++ src/components/IconRenderer.tsx
@@ -0,0 +1,54 @@
+import type { DOMNode } from 'html-react-parser'
+
+import parse, { attributesToProps, domToReact, Element } from 'html-react-parser'
+import React from 'react'
+
+interface IconRendererProps extends React.SVGProps<SVGSVGElement> {
+  color?: string
+  size?: number | string
+  svgString: string
+}
+
+export const IconRenderer: React.FC<IconRendererProps> = ({
+  className,
+  color,
+  size,
+  svgString,
+  ...restProps
+}) => {
+  if (!svgString) {
+    return null
+  }
+
+  return (
+    <>
+      {parse(svgString, {
+        replace: (domNode) => {
+          if (domNode instanceof Element && domNode.name === 'svg') {
+            const props = attributesToProps(domNode.attribs)
+
+            return React.createElement(
+              'svg',
+              {
+                ...props,
+                className: [props.className, className].filter(Boolean).join(' '),
+                ...(size && { height: size, width: size }),
+                ...(color && {
+                  fill:
+                    props.fill === 'currentColor' || (props.fill && props.fill !== 'none')
+                      ? color
+                      : props.fill,
+                  stroke: props.stroke && props.stroke !== 'none' ? color : props.stroke,
+                }),
+                ...restProps,
+              },
+              domToReact(domNode.children as DOMNode[]),
+            )
+          }
+        },
+      })}
+    </>
+  )
+}
+
+export default IconRenderer
--- src/exports/client.ts
+++ src/exports/client.ts
@@ -1,3 +1,4 @@
 export { IconCell } from '../components/IconCell.js'
 export { IconPackProvider } from '../components/IconPackContext.js'
 export { IconPicker } from '../components/IconPicker.js'
+export { IconRenderer } from '../components/IconRenderer.js'
